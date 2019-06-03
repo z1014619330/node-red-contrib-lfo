35cbc168c8baaff280f88e1d9ff95067237f3b77
dcs0spb <s.p.bradley@durham.ac.uk>
2019-06-03 18:14:00 +0100

    control offset/amplitude or min/max via configuration or messages

--- lfo/lfo.js
+++ lfo/lfo.js
@@ -2,27 +2,39 @@ module.exports = function (RED) {
     function LFONode (config) {
         const osc = require('oscillators');
         const clock = require('since-when');
+        const nrc = require('../../node-red-contrib-configure/node-red-configure');
 
         RED.nodes.createNode(this, config);
         var node = this;
         node.lfo = null;
 
-        setWaveform(config.waveform);
-        node.frequency = config.frequency;
-        node.samplingrate = config.samplingrate;
 
+        let configuration = new nrc.NodeRedConfigure(node, config,
+            {
+                name: {value:""},
+                waveform: {value: "sine"},
+                frequency: {value: 1},
+                samplingrate: {value: 20},
+                range: {value: "offsetamplitude"},
+                offset: {value: 0},
+                amplitude: {value: 1},
+                min: {value: -1},
+                max: {value: 1}
+            });
+
+        configuration.handle(setWaveform, 'waveform');
+        setWaveform(config.waveform || 'sine');
+        configuration.postHandle(setScale);
+        configuration.postHandle(function (){ node.range = 'minmax'; setScale();}, ['min', 'max']);
+        configuration.postHandle(function (){ node.range = 'offsetamplitude'; setScale();}, ['offset', 'amplitude']);
         setScale();
-
+        
         node.on('input', function (msg) {
-            if (!isNaN(msg.payload)) {
-                node.frequency = msg.payload;
-                return;
+            if (!isNaN(msg.payload) && !msg.topic) {
+                msg.topic = 'frequency';
             }
 
-            if (msg.hasOwnProperty('waveform')) {
-                setWaveform(msg.waveform);
-                return;
-            }
+            configuration.input(msg);
 
             if (msg.payload === 'stop') {
                 if (node.lfo) {
@@ -32,7 +44,7 @@ module.exports = function (RED) {
                 return;
             }
 
-            if (!node.lfo) {
+            if (msg.payload ==='start' && !node.lfo) {
                 node.time = new clock();
                 node.lfo = setInterval(function () {
                     if (!(node.oscillator instanceof Function)) {
@@ -40,7 +52,7 @@ module.exports = function (RED) {
                         return;
                     }
                     let oscBase = node.oscillator(node.time.sinceBeginNS() / 1e9, node.frequency);
-                    msg.payload = node.offset + node.amplitude * oscBase;
+                    msg.payload = node.offset_ + node.amplitude_ * oscBase;
                     node.send(msg);
                 }, node.samplingrate);
             }
@@ -70,7 +82,7 @@ module.exports = function (RED) {
         }
 
         function setScale () {
-            if(!node.range) {
+            if (!node.range) {
                 node.range = config.range;
             }
             switch (node.range) {
@@ -78,30 +90,32 @@ module.exports = function (RED) {
                 node.min = firstNumber(node.min, config.min, -1);
                 node.max = firstNumber(node.max, config.max, 1); ;
 
-                node.offset = (node.min + node.max) / 2;
-                node.amplitude = node.max - node.offset;
+                node.offset_ = (node.min + node.max) / 2;
+                node.amplitude_ = node.max - node.offset_;
                 break;
 
             case 'offsetamplitude':
             default:
-                node.offset = firstNumber(node.offset, config.offset, 0);
-                node.amplitude = firstNumber(node.amplitude, config.amplitude, 1);
+                node.offset_ = firstNumber(node.offset, config.offset, 0);
+                node.amplitude_ = firstNumber(node.amplitude, config.amplitude, 1);
             }
         }
 
         function defaultNumber (a, b) {
             // JS is weird: isNaN("") is false
-            if((typeof a) === "string"){
+            if ((typeof a) === 'string') {
                 a = a.trim();
-                if (a.length === 0);
-                return b;
+                if (a.length === 0) {
+                    return b;
+                }
             }
             let aa = Number(a);
-            return isNaN(aa) ? Number(b) : aa;
+            let result = isNaN(aa) ? Number(b) : aa;
+            return result;
         }
 
         function firstNumber (...theArgs) {
-            return theArgs.reduce((previous, current) => defaultNumber(previous, current));
+            return theArgs.reduce(function(previous, current){ return defaultNumber(previous, current)});
         }
     }
     RED.nodes.registerType('lfo-node', LFONode);
